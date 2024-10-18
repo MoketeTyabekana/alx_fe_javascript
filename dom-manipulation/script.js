bbbd3fe09ba687b76e15e13a597c9266178e4e10
Mokete <emtyabekana@mail.com>
2024-10-18 19:04:12 +0200

    Created showRandomQuote function to display a random quote from the quotes array

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,3 +5,13 @@ let quotes = [
     { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", category: "Success" },
     { text: "Life is what happens when you're busy making other plans.", category: "Life" }
   ];
+
+
+  function showRandomQuote() {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+   
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    
+    quoteDisplay.innerHTML = `<p>"${randomQuote.text}" - Category: ${randomQuote.category}</p>`;
+  }
