8cc5b8d3dd37bc98f2274d3335f241ea38f8a930
Mokete <emtyabekana@mail.com>
2024-10-18 19:01:12 +0200

    Initialized quotes array to store pre-defined quotes with text and category properties

---
+++ dom-manipulation/script.js
@@ -0,0 +1,7 @@
+
+
+let quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
+    { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", category: "Success" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" }
+  ];
