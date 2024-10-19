091cafa3704813b2bb5ac83151dfeb41ab1df9bb
Mokete <emtyabekana@mail.com>
2024-10-19 14:37:07 +0200

    Created an inportFromJSONFile function to retrieve data from the JSON file.

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -101,6 +101,20 @@ function exportToJson() {
 }
 
 
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes); 
+    saveQuotes(); localStorage
+    alert('Quotes imported successfully!');
+  };
+
+  fileReader.readAsText(event.target.files[0]);
+}
+
+
 window.onload = function() {
   showRandomQuote();
   createAddQuoteForm();
